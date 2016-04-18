22b28ab82eacba8a196eb4c9fad49a7bf79a21e8
dresosh <contact@dresosh.com>
2016-04-17 21:36:11 -0700

    adds fringe page

--- templates/found-template.php
+++ templates/found-template.php
@@ -3,10 +3,12 @@
  * Template Name: Found
  */
 ?>
-<section class="found-container container-fluid animated bounceIn">
-  <div class="row">
-    <div class="img-container animated ">
-      <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/coming_soon_flipoff.jpg" alt="" />
+<section class="found-container animated bounceIn">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">        
+        <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/coming_soon_flipoff.jpg" alt="" />
+      </div>
     </div>
   </div>
 </section>
--- templates/fringe-template.php
+++ templates/fringe-template.php
@@ -3,10 +3,12 @@
  * Template Name: Fringe
  */
 ?>
-<section class="fringe-container container-fluid animated bounceInLeft">
-  <div class="row">
-    <div class="img-container animated rollIn">
-      <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/coming_soon_flipoff.jpg" alt="" />
+<section class="fringe-container animated bounceInLeft">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        test
+      </div>
     </div>
   </div>
 </section>
