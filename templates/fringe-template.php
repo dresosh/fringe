8256838803cb3c493ef16e68d1a2fed7272ff1a2
dresosh <contact@dresosh.com>
2016-04-19 00:13:46 -0700

    adds date header and blog content to fringe page

--- templates/fringe-template.php
+++ templates/fringe-template.php
@@ -5,27 +5,33 @@
 ?>
 <section class="fringe-container animated bounceInLeft">
   <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-          <!-- Indicators -->
-          <ol class="carousel-indicators">
-            <!-- Slide Buttons go here -->
-          </ol>
+    <?php $args = array( 'post_type' => 'fringe', 'posts_per_page' => 10 ); ?>
+    <?php $loop = new WP_Query( $args ); ?>
+    <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+      <div class="row">
+        <div class="col-md-offset-2 col-md-8">
+          <h5><?php the_date(); ?></h5>
+          <h3><?php the_title(); ?></h3>
+          <br>
+          <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <!-- <ol class="carousel-indicators"> -->
+              <!-- Slide Buttons go here -->
+            <!-- </ol> -->
 
-          <!-- Wrapper for slides -->
-          <div class="carousel-inner" role="listbox">
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
 
-            <?php
-            $images = get_field('gallery');
-            if( $images ): ?>
+              <?php
+              $images = get_field('gallery');
+              if( $images ): ?>
 
-            <?php foreach( $images as $image ): ?>
-              <div class="item">
-                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-                <!-- <p><?php echo $image['caption']; ?></p> -->
-              </div>
-            <?php endforeach; ?>
+              <?php foreach( $images as $image ): ?>
+                <div class="item">
+                  <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                  <!-- <p><?php echo $image['caption']; ?></p> -->
+                </div>
+              <?php endforeach; ?>
 
             <?php endif; ?>
 
@@ -41,7 +47,16 @@
             <span class="sr-only">Next</span>
           </a>
         </div>
+        <br>
+        <p>
+          <?php echo get_field('blog'); ?>
+        </p>
+        <br>
+        <h4>Read More</h4>
+        <hr>
       </div>
     </div>
+    <?php endwhile; ?>
+
   </div>
 </section>
