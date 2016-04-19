fdc2e877255632935bffe91a5eef23f338fba6df
dresosh <contact@dresosh.com>
2016-04-19 00:42:54 -0700

    fixes thumbnail image size on facts page

--- templates/facts-template.php
+++ templates/facts-template.php
@@ -19,8 +19,8 @@
           <?php $args = array( 'post_type' => 'facts', 'posts_per_page' => 10 ); ?>
           <?php $loop = new WP_Query( $args ); ?>
           <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-            <div class="col-md-4 col-sm-4">
-              <img class="img-responsive" src="<?php the_post_thumbnail_url(); ?>" alt="" />
+            <div class="facts-thumb col-md-4 col-sm-4">
+              <img src="<?php the_post_thumbnail_url(); ?>" alt="" />
             </div>
           <?php endwhile; ?>
         </div>
